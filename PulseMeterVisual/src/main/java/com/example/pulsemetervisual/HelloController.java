dce56f457efac9fe0e0e7da01f4d893663eb2306
xma4602 <xma4602@gmail.com>
2022-12-21 18:09:35 +0400

    добавлен модуль PulseMeterVisual для окна программы

---
+++ PulseMeterVisual/src/main/java/com/example/pulsemetervisual/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.pulsemetervisual;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
